9b233a4be997f03a51e9e261fdc37b1b4088475c
ofir hodara <ofir1682002@gmail.com>
2022-02-06 21:33:13 +0200

    try first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Ofir the gever
         </a>
       </header>
     </div>
