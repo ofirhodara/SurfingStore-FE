c3144a2fb0792dcb39548b796e031cc0d841274e
ofir hodara <ofir1682002@gmail.com>
2022-02-20 12:30:58 +0200

    end layout

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import bg_surfing from '../../assets/bg_surfing.jpg';
 import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>Surfing Store</h1>
+        <HeaderCartButton></HeaderCartButton>
       </header>
 
       <div className={classes['main-image']}>
